30b571df013e94887e3dab67ba6449c9a5aeb7ad
Sami Ullah <samiullahk997@gmail.com>
2024-10-24 12:22:35 +0500

    some ui changes done.

--- app/profile/[[...profile]]/page.tsx
+++ app/profile/[[...profile]]/page.tsx
@@ -1,19 +1,17 @@
 import { UserProfile } from "@clerk/nextjs";
-import { auth, currentUser } from "@clerk/nextjs/server";
+import { auth } from "@clerk/nextjs/server";
 import React from "react";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
   const { userId } = await auth();
   const isAuth = !!userId;
-  const user = await currentUser();
 
     if (!isAuth) {
         redirect("/");
     }
   return (
     <div className="flex flex-col items-center justify-center h-full mt-8">
-      <h1 className="text-2xl">{user?.username}</h1>
       <UserProfile />
     </div>
   );
