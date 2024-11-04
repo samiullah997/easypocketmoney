ab50a8ba305e3c5360bb93786eb1c3d14950b2c3
Sami Ullah <samiullahk997@gmail.com>
2024-11-04 16:42:31 +0500

    fix after sign url

--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,9 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
+const NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL = process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL;
 
 export default function Page() {
   return (
     <div className="flex items-center justify-center h-full">
-      <SignUp afterSignOutUrl="/"/>
+      <SignUp afterSignOutUrl={NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL}/>
     </div>
   );
 }
